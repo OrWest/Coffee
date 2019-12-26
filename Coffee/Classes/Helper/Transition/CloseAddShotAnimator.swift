d7964d042823db38c1b689a5b0f36d0c00251ac6
Alexander Motarykin <aliaksandr_matarykin@epam.com>
2019-12-26 16:39:28 +0300

    Added animation to dismiss (add and cance).

---
+++ Coffee/Classes/Helper/Transition/CloseAddShotAnimator.swift
@@ -0,0 +1,9 @@
+//
+//  CloseAddShotAnimator.swift
+//  Coffee
+//
+//  Created by Aliaksandr Matarykin on 12/26/19.
+//  Copyright © 2019 Alexander Motarykin. All rights reserved.
+//
+
+import Foundation
