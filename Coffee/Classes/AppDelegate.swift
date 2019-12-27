996c909e42d98941c337437ba305147596063ec4
Alexander Motarykin <aliaksandr_matarykin@epam.com>
2019-12-27 14:25:57 +0300

    Did project to use scene (iOS 13)

--- Coffee/Classes/AppDelegate.swift
+++ Coffee/Classes/AppDelegate.swift
@@ -15,14 +15,12 @@ import RealmSwift
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         initializeServices()
         return true
     }
-    
+
     private func initializeServices() {
         FirebaseApp.configure()
 
@@ -60,6 +58,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         } catch {
             print("Realm error: \(error)")
         }
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
 
