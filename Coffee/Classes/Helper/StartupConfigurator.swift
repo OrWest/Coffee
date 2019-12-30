8b75fa4a02a3f627b1a5a0b1c7ac766d7af13f1b
Alexander Motarykin <aliaksandr_matarykin@epam.com>
2019-12-30 15:14:34 +0300

    Added file name for config.

--- Coffee/Classes/Helper/StartupConfigurator.swift
+++ Coffee/Classes/Helper/StartupConfigurator.swift
@@ -10,14 +10,14 @@ import Foundation
 import FirebaseStorage
 
 class StartupConfigurator {
-    private let storageRef = Storage.storage().reference()
+    private let storageRef = Storage.storage().reference(withPath: "startup-coffee-info.json")
 
     func downloadStartupInfo(_ completion: (Result<[Coffee], Error>) -> Void) {
 
         // 1 MB (2^16)
         storageRef.getData(maxSize: 2 << 16) { data, error in
             if let data = data {
-                
+
             } else if let error = error {
                 completion(Result.failure(error))
             } else {
