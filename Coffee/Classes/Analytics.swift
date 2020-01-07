2daa98ecc443afdd04ced1b07763b5a85949b4da
Alexander Motarykin <wwweshka@rambler.ru>
2020-01-07 16:09:37 +0300

    Added analytics class.

---
+++ Coffee/Classes/Analytics.swift
@@ -0,0 +1,14 @@
+//
+//  Analytics.swift
+//  Coffee
+//
+//  Created by Alex Motor on 1/7/20.
+//  Copyright © 2020 Alexander Motarykin. All rights reserved.
+//
+
+import Foundation
+import FirebaseAnalytics
+
+class Analytics {
+    
+}
